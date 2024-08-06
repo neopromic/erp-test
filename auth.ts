aa39deae86e2482ad4cd22c962b0a02eb3373d8e
Weslley S. | Neo <cusaowesley1@gmail.com>
2024-08-06 11:53:04 -0300

    feat: setup prisma

--- auth.ts
+++ auth.ts
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
+const prisma = new PrismaClient()
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 })
